3cb6da8ff31fe76d41830ef58e41a22209c4b294
Crunchy <masonsandersbussiness@gmail.com>
2026-09-13 16:13:05 +0100

    Add files via upload

---
+++ OneWeb-config.js
@@ -0,0 +1,14 @@
+// Set the SAME new project values in all five apps. Never put a secret/service-role key here.
+window.ONELEARN_CONFIG = {
+  "product": "OneWeb",
+  "supabaseUrl": "https://nndjwqjyfgozltdoujiu.supabase.co",
+  "supabaseKey": "sb_publishable_fWD8fxrR9-u1oslITaCZhw_WAooeP69",
+  "apps": {
+    "OneLearn": "../OneLearn/",
+    "OneEducation": "../OneEducation/",
+    "OneHome": "../OneHome/",
+    "OneWeb": "../OneWeb/",
+    "OneSixth": "../OneSixth/"
+  }
+};
+
